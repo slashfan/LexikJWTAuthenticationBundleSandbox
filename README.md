4930bdfbd80b6f7a0c826f7fdca4742569fc4be2
Nicolas Cabot <slashfan@live.fr>
2014-07-22 23:05:07 +0200

    added angularjs demo

--- README.md
+++ README.md
@@ -19,3 +19,8 @@ A minimalist sandbox to quickly test JWT authentication through LexikJWTAuthenti
 ## Running Tests
 
     bin/phpunit -c app
+
+## AngularjS Demo
+
+A demo application based on [this post](http://www.kdmooreconsulting.com/blogs/authentication-with-ionic-and-angular-js-in-a-cordovaphonegap-mobile-web-application/) is available in the web/angular-demo directory.
+To run it, install the assets using `bower install` and browse to the index.html (for the example to work, both the api and the client app must be in the same domain).
---
+++ web/angular-demo/app.js
@@ -0,0 +1,77 @@
+(function () {
+
+    'use strict';
+
+    angular
+
+        .module('demoApp', ['http-auth-interceptor', 'ui.bootstrap'])
+
+        .factory('AuthenticationService', function ($rootScope, $http, authService, $httpBackend) {
+            return {
+                login:          function (credentials) {
+                    $http
+                        .post('/api/login_check', credentials, { ignoreAuthModule: true })
+                        .success(function (data, status, headers, config) {
+                            $http.defaults.headers.common.Authorization = 'Bearer ' + data.token;  // Step 1
+                            authService.loginConfirmed(data, function (config) {  // Step 2 & 3
+                                config.headers.Authorization = 'Bearer ' + data.token;
+                                $rootScope.$broadcast('event:auth-login-complete');
+                                return config;
+                            });
+                        })
+                        .error(function (data, status, headers, config) {
+                            $rootScope.$broadcast('event:auth-login-failed', status);
+                        });
+                },
+                logout:         function (user) {
+                    delete $http.defaults.headers.common.Authorization;
+                    $rootScope.$broadcast('event:auth-logout-complete');
+                }
+            };
+        })
+
+        .controller('AppCtrl', function ($scope, $modal) {
+
+            $scope.$on('event:auth-loginRequired', function () {
+                $modal.open({
+                    templateUrl: 'login.html',
+                    controller:  'ModalInstanceCtrl',
+                    backdrop:    'static'
+                });
+            });
+
+        })
+
+        .controller('ModalInstanceCtrl', function ($scope, $modalInstance, AuthenticationService) {
+
+            $scope.credentials = {
+                username: 'user',
+                password: 'password'
+            };
+
+            $scope.$on('event:auth-login-failed', function () {
+                $scope.errorMessage = 'Bad credentials';
+            });
+
+            $scope.$on('event:auth-login-complete', function () {
+                $modalInstance.close();
+            });
+
+            $scope.submit = function (credentials) {
+                AuthenticationService.login(credentials);
+            };
+
+        })
+
+        .controller('MainCtrl', function ($scope, $http) {
+            $http
+                .get('/api/pages')
+                .then(function (response) {
+                    $scope.pages = response.data;
+                })
+            ;
+        })
+
+    ;
+
+})();
